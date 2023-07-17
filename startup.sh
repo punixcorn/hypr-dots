718f17c0bcf3fe5bff8bcded9d5646195863d2dc
potatoJs <103060805+potato-Js@users.noreply.github.com>
2023-07-17 14:54:38 +0000

    made some changes

--- startup.sh
+++ startup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-#live wallpaper
-LiveWallpaper="$HOME/.config/hypr/wallpaper/sasuke.gif"
+# convert to GIF
+# ffmpeg -i input.mp4 -vf scale=320:-1 -r 10 -f image2pipe -vcodec ppm - | convert -delay 10 -loop 0 - output.gif
+
+# -- live wallpaper [ Needs gif ]-- #
+# -- Sww -- #
+# LiveWallpaper="$HOME/.config/hypr/wallpaper/sasuke.gif"
+LiveWallpaper="$HOME/Videos/ethel_1.mp4"
 swww img ${LiveWallpaper}
 
+# -- normal wallpaper -- #
+# -- swaybg -- #
+# swaybg -i <path-to-img> --mode tile
 #clipman >> clipboard
 exec wl-paste -p -t text --watch clipman store -P --histpath="~/.local/share/clipman-primary.json"
